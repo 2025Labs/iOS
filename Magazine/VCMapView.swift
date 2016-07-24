a176a1f5d11ac5074dffd88f7cd2029e1db5e467
Richard Pham <hellorichardpham@gmail.com>
2016-07-24 13:56:26 -0700

    Modified Callout so popover appears on click

--- Magazine/VCMapView.swift
+++ Magazine/VCMapView.swift
@@ -27,18 +27,76 @@ extension ViewController: MKMapViewDelegate {
                 // 3
                 view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
                 view.canShowCallout = true
-                view.calloutOffset = CGPoint(x: -5, y: 5)
+                //view.calloutOffset = CGPoint(x: -5, y: 5)
             
-                view.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure) as UIView
+                //view.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure) as UIView
             }
             return view
         }
         return nil
     }
 
+    func mapView(mapView: MKMapView, didSelectAnnotationView annotationView: MKAnnotationView)
+    {
     
+        let pin = annotationView.annotation
+        mapView.deselectAnnotation(pin, animated: false)
+        
+        
+    var width = self.view.frame.width / 3
+    if(self.view.frame.height > self.view.frame.width) {
+    width = self.view.frame.height / 3
+    }
+    let annView = annotationView.annotation as? City;
+
+        
+    let aView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: width))
+    print(width)
     
+    let imageName = (annView!.image)
+    let image = UIImage(named: imageName)
+    let imageView = UIImageView(image: image)
+    
+    imageView.frame = CGRect(x: 0, y: 0, width: width, height: width/1.8)
+    aView.addSubview(imageView)
+    
+    let label = UILabel(frame: CGRect(x:0, y:width/2, width: width, height: width/2))
+    label.text = (annView!.information)
+    label.numberOfLines = 0
+    label.textAlignment = NSTextAlignment.Center
+    label.font = label.font.fontWithSize(12)
+    aView.addSubview(label);
+    var options:[PopoverOption]
+    
+    if(annotationView.frame.origin.y - width < 15) {
+    options = [
+    .Type(.Down),
+    .AnimationIn(0.3),
+    .ArrowSize(CGSize(width: 15.0, height: 15.0))
+    ] as [PopoverOption]
+    
+    } else {
+    options = [
+    .Type(.Up),
+    .AnimationIn(0.3),
+    .ArrowSize(CGSize(width: 15.0, height: 15.0))
+    ] as [PopoverOption]
+    }
+    
+    let popover = Popover(options: options, showHandler: nil, dismissHandler: nil)
+    
+    print(annotationView.frame.origin.x)
+    print(annotationView.frame.origin.y)
+    
+    popover.show(aView, fromView: annotationView)
+    
+}
+
+
+
     func mapView(mapView: MKMapView, annotationView: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        
+       
         let annView = annotationView.annotation as? City;
         print("Information: " + annView!.information);
         print("Image: %s", annView!.image);
